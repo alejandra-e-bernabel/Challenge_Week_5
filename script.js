4530a1b0f0ef9144249b5b9fbf51e9dbbc8e318c
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-04 01:08:52 -0400

    Updated formatting of time and added current date. Also changed some styling to personalize scheduler.

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-
-
 $(document).ready(function () {
 
   var blockElements = [
@@ -19,10 +11,16 @@ $(document).ready(function () {
     $("#hour-4"),
     $("#hour-5"),
   ];
+
+
   
   setInterval(function(){
     currTime = dayjs();
-    $("#currentDay").text("Current time is " + currTime.$H + ":" + currTime.$m);
+
+    //prints out current time and date in specificed format
+
+    $("#currentDate").text ("Today's date is " + currTime.format ("MMM D, YYYY"));
+    $("#currentTime").text("Current time is " + currTime.format("h:mm A"))
 
     // loop that checks whether boxes should be set for past, present, or future    
     for (var i=0; i<9; i++) {
@@ -71,12 +69,8 @@ function setFuture (hourIndex) {
   }
 }
   
-  //console.log ("Current time is " + currTime);
-  //$("header").append("Current time is " + dayjs().$H + ":" + dayjs().$m);
-
-  //TODO fix formatting on this and update it so it shows AM and PM rather than 24 hours.
-  //A simple if statement will do. If time is greater than 12, ...
-  
+//these if statements make it so that when the page is regreshed, all text box content is retrieved
+//from local memory and printed back to the calendar in the correct location
 
   if (localStorage.getItem("hour-9") != null) {
     $("#hour-9-button").text(localStorage.getItem("hour-9"));
@@ -125,7 +119,7 @@ function setFuture (hourIndex) {
 
 
 
-
+//buttons to reset scheduler and save new events to local memory.
   $(".saveBtn").click(function(){
     //I want to save the button's sibling text to the key of the box class
     console.log ("Button was pressed");
@@ -137,44 +131,5 @@ function setFuture (hourIndex) {
     location.reload();
   })
 
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  //I can probably use the parent function to check which button has been clicked.
-  
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
-
-
-
-// $(function checkHour (blockTime) {
-//   if (blockTime < dayjs().$H) {
-    
-//   }
-
-//   else if (blockTime == dayjs().$H) {
-
-//   }
-
-//   else {
-
-//   }
-    
-// })
 
